22ee2a0b3f127b0baebabcbd04426bc9aa562572
rcxxx <chuangxinr@gmail.com>
2023-03-31 17:01:18 +0800

    update cfg.yaml

--- auto-cat-teaser/auto_cat_teaser.py
+++ auto-cat-teaser/auto_cat_teaser.py
@@ -3,7 +3,11 @@ import asyncio
 import serial
 import logging
 
+import yaml
+import sys
 from yolov8_onnx import Model
+
+cfg = yaml.load(open('cfg.yaml', 'r', encoding='utf-8').read(), Loader=yaml.FullLoader)
 
 async def yoloDetect(_cap, _net):
     while True:
@@ -32,7 +36,9 @@ async def yoloDetect(_cap, _net):
                                       round((detection['box'][0] + detection['box'][2]) * detection['scale']),
                                       round((detection['box'][1] + detection['box'][3]) * detection['scale']))
 
-        cv2.imshow('src', src_img)
+        row, col, _ = src_img.shape
+        roi_img = src_img[20:row - 20, 0:col]
+        cv2.imshow('src', roi_img)
         if cv2.waitKey(1) == 27:
             break
 
@@ -45,27 +51,26 @@ async def intervalSerial(_ser):
         await asyncio.sleep(SER_WRITE_INTERVAL)
         serialWrite(_ser)
 
-async def startUp(_cap_id, _model_path, _inp_size):
+async def startUp(_cap, _model_path, _inp_size):
     try:
-        ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=0.5)
-        cap = cv2.VideoCapture(_cap_id)
+        ser = serial.Serial(port=cfg['serial_port'], baudrate=115200, timeout=0.5)
+        cap = cv2.VideoCapture(_cap)
         net = Model(_model_path, _size=_inp_size)
         task_yolo = asyncio.create_task(yoloDetect(_cap= cap, _net= net))
         task_serial = asyncio.create_task(intervalSerial(_ser= ser))
-        await asyncio.wait({task_yolo})
+        await asyncio.wait({task_yolo, task_serial})
     except:
         logging.warning("Failed to open the serial port, cancel sending")
-        cap = cv2.VideoCapture(_cap_id)
+        cap = cv2.VideoCapture(_cap)
         net = Model(_model_path, _size=_inp_size)
         task_yolo = asyncio.create_task(yoloDetect(_cap= cap, _net= net))
         await asyncio.wait({task_yolo})
-
 
 if __name__ == '__main__':
     try:
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(startUp(_cap_id=0,
-                                        _model_path='./models/yolov8s-480x.onnx',
+        loop.run_until_complete(startUp(_cap=cfg['cap'],
+                                        _model_path=sys.path[0] + '/models/yolov8s-480x.onnx',
                                         _inp_size = (480,480)))
 
     except KeyboardInterrupt as exc:
